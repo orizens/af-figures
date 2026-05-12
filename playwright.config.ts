bd69858fbc843f49d73e27efae7e0c8d07bc872d
Oren Farhi <farhioren@gmail.com>
2026-05-12 19:55:49 -0400

    use 127 for lcoal

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,13 +4,13 @@ export default defineConfig({
   testDir: './tests/e2e',
   fullyParallel: true,
   forbidOnly: !!process.env['CI'],
-  retries: process.env['CI'] ? 0 : 0,
+  retries: process.env['CI'] ? 1 : 0,
   workers: process.env['CI'] ? 1 : undefined,
   reporter: process.env.CI
     ? [['blob'], ['line']]
     : [['html', { open: 'never' }], ['line']],
   use: {
-    baseURL: 'http://localhost:4200',
+    baseURL: 'http://127.0.0.1:3200',
     trace: 'retain-on-failure',
   },
   projects: [
@@ -20,8 +20,8 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'bun run dev --host 0.0.0.0',
-    port: 4200,
+    command: 'bun run dev',
+    port: 3200,
     reuseExistingServer: !process.env.CI,
     timeout: 120_000,
     stdout: 'pipe',
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     tailwindcss(),
   ],
   server: {
-    port: 4200,
+    port: 3200,
     strictPort: true,
     host: '0.0.0.0',
   },
