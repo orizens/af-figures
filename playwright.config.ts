864e3d2b609de435410d77dc23cef9d8f02ef447
Oren Farhi <farhioren@gmail.com>
2026-05-12 21:35:01 -0400

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   retries: process.env['CI'] ? 1 : 0,
   workers: process.env['CI'] ? 1 : undefined,
   reporter: process.env.CI
-    ? [['blob'], ['line']]
+    ? [['html'], ['github'], ['line']]
     : [['html', { open: 'never' }], ['line']],
   use: {
     baseURL: 'http://127.0.0.1:3200',
@@ -19,12 +19,12 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-  webServer: {
-    command: 'bun run dev',
-    port: 3200,
-    reuseExistingServer: !process.env.CI,
-    timeout: 120_000,
-    stdout: 'pipe',
-    stderr: 'pipe',
-  },
+  // webServer: {
+  //   command: 'bun run dev --host',
+  //   port: 3200,
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 120_000,
+  //   stdout: 'pipe',
+  //   stderr: 'pipe',
+  // },
 })
